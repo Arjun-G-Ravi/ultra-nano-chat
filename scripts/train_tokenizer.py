51f4ecb1a157dbc02ca9e5132bf57f8c998450fe
Arjun-G-Ravi <arjungravi1@gmail.com>
2026-06-09 00:12:35 +0530

    Patch

--- scripts/train_tokenizer.py
+++ scripts/train_tokenizer.py
@@ -5,7 +5,7 @@ from collections import Counter
 with open('data/validation.txt') as f:
     text = f.read()
 
-text = text[:100]
+text = text[:1000]
 tokens = text.encode('utf-8')
 tokens = list(map(int, tokens))
 # print(tokens)
@@ -13,7 +13,7 @@ vocab = {chr(i):i for i in range(32, 127)} # basic characters
 vocab_ct = 127
 # print('\nvocab', vocab, '\n')
 
-vocab_size = 1024
+vocab_size = 256
 
 while vocab_ct < vocab_size:
     combined_tokens = []
@@ -23,16 +23,22 @@ while vocab_ct < vocab_size:
     ch1, ch2 = str(counter.most_common(1)[0][0]).split('_')
     # print(ch1,ch2)
     vocab[f'{chr(int(ch1))}{chr(int(ch2))}'] = vocab_ct
+
+    l1 = len(tokens)
+
+    index_to_pop = []
+    for i in range(len(tokens)-1):
+        # print(tokens[i], ch1)
+        if tokens[i] == int(ch1) and tokens[i+1] == int(ch2):
+            tokens[i] = vocab_ct
+            index_to_pop.append(i+1)
+    for i in index_to_pop[::-1]:
+        tokens.pop(i)
+    l2 = len(tokens)
+
     vocab_ct += 1
+    print(tokens)
+    assert l1 == int(counter.most_common(1)[0][1])+l2, 'wrong count'
 
-    print(vocab)
-
-
-
-
-
-    break
-
-        
-
-
+with open('out/tokenizeer.txt', 'w') as f:
+    f.write(str(vocab))
