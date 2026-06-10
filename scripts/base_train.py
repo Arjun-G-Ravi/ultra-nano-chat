ec61560e9bc022ca50ef76687697d42908e18d6e
Arjun-G-Ravi <arjungravi1@gmail.com>
2026-06-10 09:50:05 +0530

    wrote attn

--- scripts/base_train.py
+++ scripts/base_train.py
@@ -1,7 +1,72 @@
 import torch
+import math
 import torch.nn as nn
+from dataclasses import dataclass
+from torch.nn import functional as F
+
+
+class CausalSelfAttention(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        self.c_proj= nn.Linear(config.n_embd, config.n_embd)
+        self.n_head = config.n_head
+        self.n_embd= config.n_embd
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                             .view(1, 1, config.block_size, config.block_size))
+
+    def forward(self, x):
+        B, T, C = x.size()
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+        k = k.view(B, T,self.n_head, C//self.n_head).transpose(1,2)
+        q = q.view(B, T,self.n_head, C//self.n_head).transpose(1,2)
+        v = v.view(B, T,self.n_head, C//self.n_head).transpose(1,2)
+
+        att = (q @ k.transpose (-2, -1)) * (1.0/ math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
+        att = F.softmax(att, dim=-1)
+        y = att @ v
+        y = y.transpose(1,2).contiguous().view(B, T, C)
+        y = self.c_proj(y)
+        return y
+
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu = nn.GELU(approximate='tanh')
+        self.c_proj = nn.Linear(4 * config.n_embd, config.embd)
+
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        self.ln2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+         
+
+@dataclass
+class GPTConfig:
+    block_size: int = 256
+    vocab_size: int = 65
+    n_layer: int = 6
+    n_head: int = 6
+    n_embd: int = 384
+
 
 class GPT(nn.Module):
     def __init__(self, config):
         super.__init__()
         self.config = config
+
+        self.transformer = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd), 
+            wpe = nn.Embedding(config.block_size, config.n_embd),
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layers)]), # the blocj that is repeated N times
+            ln_f = nn.LayerNorm(config.n_embd),
+        ))
+
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
